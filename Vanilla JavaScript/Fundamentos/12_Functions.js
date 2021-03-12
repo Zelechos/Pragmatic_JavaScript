e6ef0fea2f477c934178a19c47ad89919ef726a1
Zelechos <zelechos@hotmail.com>
2021-03-11 23:26:03 -0400

    Version 0.0.7

--- Vanilla JavaScript/Fundamentos/12_Functions.js
+++ Vanilla JavaScript/Fundamentos/12_Functions.js
@@ -1,77 +1,44 @@
 'use strict'
-
-
-function Menu(Mostrar =  false){
-  do{
-    var Opcion = Number(prompt(" ---------- MENU----------- \n1.- Sumar \n2.- Restar\n3.- Multiplicar\n4.- Dividir\n 5.-Salir \n Elige una Opcion :\n"));
-
-      switch(Opcion){
-          case 1:
-          var Number1 = parseInt(prompt("Para Sumar\n\nDigite un Numero : ",0));
-          var Number2 = parseInt(prompt("Digite otro Numero : ",0));
-          alert("La suma es : "+Sumar(Number1,Number2));
-          break;
-          case 2:
-          var Number1 = parseInt(prompt("Para Restar\n\nDigite un Numero : ",0));
-          var Number2 = parseInt(prompt("Digite otro Numero : ",0));
-          alert("La resta es : "+Restar(Number1,Number2));
-          break;
-          case 3:
-          var Number1 = parseInt(prompt("Para Multiplicar\n\nDigite un Numero : ",0));
-          var Number2 = parseInt(prompt("Digite otro Numero : ",0));
-          alert("La Multiplicacion es : "+Multiplicar(Number1,Number2));
-          break;
-          case 4:
-          var Number1 = parseInt(prompt("Para Dividir\n\nDigite un Numero : ",0));
-          var Number2 = parseInt(prompt("Digite otro Numero : ",0));
-          alert("LA Division es : "+Dividir(Number1,Number2));
-          break;
-          case 5:
-          alert("Fin del Programa");
-          break;
-          default:
-          alert("No existe es Opcion en el Menu Intenete de nuevo . ");
-          break;
-    }
-  }while(Opcion != 5);
-
-
-}
+let Opcion;
 function Menu(){
   do{
-    var Opcion = Number(prompt(" ---------- MENU----------- \n1.- Sumar \n2.- Restar\n3.- Multiplicar\n4.- Dividir\n 5.-Salir \n Elige una Opcion :\n"));
+    let Opcion = Number(prompt(" ---------- MENU----------- \n1.- Sumar \n2.- Restar\n3.- Multiplicar\n4.- Dividir\n 5.-Salir \n Elige una Opcion :\n"));
 
       switch(Opcion){
           case 1:
-          var Number1 = parseInt(prompt("Para Sumar\n\nDigite un Numero : ",0));
-          var Number2 = parseInt(prompt("Digite otro Numero : ",0));
-          document.write("La suma es : "+Sumar(Number1,Number2));
+            const [Number1, Number2] = Data();
+            alert("La suma es : "+Sumar(Number1,Number2));
           break;
           case 2:
-          var Number1 = parseInt(prompt("Para Restar\n\nDigite un Numero : ",0));
-          var Number2 = parseInt(prompt("Digite otro Numero : ",0));
-          document.write("La resta es : "+Restar(Number1,Number2));
+            const [Number3, Number4] = Data();
+            alert("La resta es : "+Restar(Number3,Number4));
           break;
           case 3:
-          var Number1 = parseInt(prompt("Para Multiplicar\n\nDigite un Numero : ",0));
-          var Number2 = parseInt(prompt("Digite otro Numero : ",0));
-          document.write("La Multiplicacion es : "+Multiplicar(Number1,Number2));
+            const [Number5, Number6] = Data();
+            alert("La Multiplicacion es : "+Multiplicar(Number5,Number6));
           break;
           case 4:
-          var Number1 = parseInt(prompt("Para Dividir\n\nDigite un Numero : ",0));
-          var Number2 = parseInt(prompt("Digite otro Numero : ",0));
-          document.write("LA Division es : "+Dividir(Number1,Number2));
+            const [Number7, Number8] = Data();
+            alert("La Division es : "+Dividir(Number7,Number8));
           break;
           case 5:
-          alert("Fin del Programa");
+            if(confirm("Estas seguro de que quieres salir?")){
+              this.close();//para cerrar la ventana emergente que sale del navegardor
+            }
           break;
           default:
-          alert("No existe es Opcion en el Menu Intenete de nuevo . ");
+            alert("No existe es Opcion en el Menu Intenete de nuevo . ");
           break;
     }
+    
   }while(Opcion != 5);
+}
 
 
+function Data(){
+  let Number1 = parseInt(prompt("Digite un Numero : ",0));
+  let Number2 = parseInt(prompt("Digite otro Numero : ",0));
+  return [Number1 , Number2];
 }
 
 function Sumar(Number1 ,Number2){
@@ -90,10 +57,10 @@ function Dividir(Number1,Number2){
   return Number1/Number2;
 }
 
-
-Menu(true);
-
 function Mensaje(){
-  console.log("Mensaje destinado a la consola");
+  alert("Mensaje destinado a la consola");
   return "Hola soy Un mensaje";
 }
+
+Menu();
+
