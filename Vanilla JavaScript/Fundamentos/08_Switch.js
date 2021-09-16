c297424fe2e6591d1ec9d35bb4133f8169f206ad
Zelechos <zelechos@hotmail.com>
2021-09-16 13:31:01 -0400

    v0.1.4

--- Vanilla JavaScript/Fundamentos/08_Switch.js
+++ Vanilla JavaScript/Fundamentos/08_Switch.js
@@ -11,54 +11,54 @@ const Messages = [
 
 let Entry = 0
 let History = []
-
-function App(){
-    Active();
-    ShowHistory(History);
-}
-
-App();
+let body = document.getElementById("content");
 
 function Active(){
-let Age = Number(prompt("Entrada "+Entry+"\n Digite su Antiguedad en el Club : "));
+    let Age = Number(prompt("Entrada "+Entry+"\n Digite su Antiguedad en el Club : "));
 
-let Response = `
-<fieldset>
-    <legend>Su Antiguedad es de ${Age}</legend>
-    <p>Entrada : ${Entry}</p>
-    <p> Rango : ${ChekerAge(Age)}</p>        
-    <input type="button" value="Agregar Entradas" onclick="App();">
-</fieldset>
-`;
+    let Response = `
+    <fieldset>
+        <legend>Su Antiguedad es de ${Age}</legend>
+        <p>Entrada : ${Entry}</p>
+        <p> Rango : ${ChekerAge(Age)}</p>        
 
-switch(Age){
-    case 18:
-        History.push(Response)
-    break;
-    case 25:
-        History.push(Response)
-    break;
-    case 40:
-        History.push(Response)
-    break;
-    case 50:
-        History.push(Response)
-    break;
-    case 75:
-        History.push(Response)
-    break;
-    default:
-        History.push(Response)
-    break;
-}
+        <input type="button" value="Agregar Entradas" onclick="App();">
+
+    </fieldset>
+    `;
+
+    switch(Age){
+        case 18:
+            History.push(Response)
+        break;
+        case 25:
+            History.push(Response)
+        break;
+        case 40:
+            History.push(Response)
+        break;
+        case 50:
+            History.push(Response)
+        break;
+        case 75:
+            History.push(Response)
+        break;
+        default:
+            History.push(Response)
+        break;
+    }
 
     Entry += 1;
+    ShowHistory(History);
 }
 
 
 function ShowHistory(Historia){
-    for(let i = 0 ; i < Historia.length ; i++){
-        document.write(Historia[i]);
+    let Iterator = Historia.length - 1;
+    console.log(Iterator);    
+    while(Iterator < Historia.length){
+        document.write(Historia[Iterator]);
+        Iterator += 1;
     }
 }
 
@@ -70,4 +70,10 @@ function ChekerAge(age){
                     else if(age == 75) return Messages[4]
                         else return Messages[5]
 }
+
+function App(){
+    Active();
+}
+
+App();
 
