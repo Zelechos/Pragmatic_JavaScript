dced8aca987f0a5c7c02049ef74c386372656711
Zelechos <zelechos@hotmail.com>
2021-10-14 19:00:33 -0400

    v0.1.8

--- Vanilla JavaScript/Fundamentos/36_ExpresionesRegulares.js
+++ Vanilla JavaScript/Fundamentos/36_ExpresionesRegulares.js
@@ -3,13 +3,17 @@
 // Expresiones Regulares muy Importante
 // repasar y estudiar mucho
 
-let text = "Argus Aphocraphex Ether Zelechos etheraphocraphex etheretheretherargusargus argus ether 9";
+let text = "Argus Aphoc1raphex Ether Zelechos etheraphocraphex etheretheretherargusargus argus ether 9";
 
 // Declarando un Objeto de expresion regulares
 let expresionRegular = new RegExp("ether", "ig");
 
 // Diferente formato el mas usado
 let expresionRegular2 = /ether{1,}/ig;
+
+// Que dice si incluye algun Numero del 0 al 9 
+let expresionRegular3 = /[0-9]/
+
 
 console.log("___________ Primera Expresion Regular_____________");
 console.log(expresionRegular.test(text));
@@ -18,4 +22,8 @@ console.log(expresionRegular.exec(text));
 console.log("___________ Segunda Expresion Regular_____________");
 console.log(expresionRegular2.test(text));
 console.log(expresionRegular2.exec(text));
+
+console.log("___________ Tecera Expresion Regular_____________");
+console.log(expresionRegular3.test(text));
+console.log(expresionRegular3.exec(text));
 
