f1ed8f8a4fcffbaef69934367f84fef5a37a2728
Zelechos <zelechos@hotmail.com>
2021-10-17 20:01:55 -0400

    v0.1.9

--- Exercises JavaScript/ExercisesJS/Exercise16.js
+++ Exercises JavaScript/ExercisesJS/Exercise16.js
@@ -1,2 +1,25 @@
-// 16) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
+// 16) Programa una función que cuente el número de caracteres de una cadena de texto, testeando el tipo de dato recibido pe. miFuncion("Hola Mundo") devolverá 10.
 'use strict'
+
+function numberCharacters (data){
+    try {
+        if(typeof(data) != "string" && data instanceof(String) == false){
+            throw new Error(` ==>Tipo de Dato ${typeof(data)} incorrecto!! `);
+        }    
+        console.log(`el numero de caracteres de "${ data }" es : ${data.length}`);
+    } catch (error) {
+        console.error(error);
+    }finally{
+        console.warn("==> Program End");
+    }
+}
+
+// Testing
+let text = new String("hola");
+numberCharacters("ether");
+numberCharacters([4,5.1]);
+numberCharacters({});
+numberCharacters(87);
+numberCharacters('s');
+numberCharacters(text);
+numberCharacters("hola soy programador")
