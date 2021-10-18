adbe13b0aa5d965086af2e397ba16840dc6ab91c
Zelechos <zelechos@hotmail.com>
2021-10-18 12:16:42 -0400

    v0.1.9

--- Exercises JavaScript/ExercisesJS/Exercise21.js
+++ Exercises JavaScript/ExercisesJS/Exercise21.js
@@ -1,2 +1,30 @@
 // 21) Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
+import validateString from './validations.js'
 'use strict'
+
+const counterWord = (data, word) =>
+    (validateString(data) && validateString(word))
+    ? console.log(`[${data}] \n word concidences {${word}} => ${finderWord(data , word)}`)
+    : console.warn(`wrong parameters => parameter{${typeof(data)}} parameter1{${typeof(word)}} \n was expected => parameter{string} parameter1{string}`);
+
+
+const finderWord = (data, word) =>{
+    let count = 0;
+    let database = data.split(" ");
+    for (let index = 0; index < database.length; index++) {
+        if(database[index] === word){
+            count++;
+        }
+    }
+    return count;
+}
+
+// Testing
+counterWord("ether hello 9 ether 9 ether","9");
+counterWord("ether hello 9 ether 9 ether","ether");
+counterWord("ether hello ether ether", 9);
+counterWord([],"ether");
+counterWord("ether hello ether ether",false);
+counterWord("ether hello ether ether");
+counterWord(0,true);
+counterWord();
--- Exercises JavaScript/ExercisesJS/Exercise23.js
+++ Exercises JavaScript/ExercisesJS/Exercise23.js
@@ -1,2 +1,29 @@
 // 23) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+import validateString from "./validations.js";
 'use strict'
+
+const destroyPattern = (data, pattern) =>
+    (validateString(data) && validateString(pattern))
+    ? console.log(`with pattern => ${data} \n without pattern => ${finderPattern(data, pattern)}`)
+    : console.warn(`wrong parameters => parameter{${typeof(data)}} parameter1{${typeof(pattern)}} \n was expected => parameter{string} parameter1{string}`) 
+
+const finderPattern = (data, pattern) => {
+    let database = data.split(" "), datafinal = [];
+    for (let index = 0; index < database.length; index++) {
+        (database[index].includes(pattern))
+        ? datafinal[index] = database[index].replace(pattern,"")
+        : datafinal[index] = database[index];
+    }
+    return String(datafinal.join(" "));
+}
+
+// Testing
+destroyPattern("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz");
+destroyPattern("https://www.ether.com , https://www.zelechos.com , https://www.aphocraphex.com", "https://www.");
+destroyPattern([],"x");
+destroyPattern("hola como estas que tienes",true);
+destroyPattern("hola como estas que tienes",9);
+destroyPattern();
+destroyPattern([]);
+destroyPattern(true);
+destroyPattern(9);
