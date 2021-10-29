453e1306a86704ecf9bf7b0bb96190f6241f6ab9
Zelechos <zelechos@hotmail.com>
2021-10-28 20:57:06 -0400

    v0.2.0

--- Exercises JavaScript/ExercisesJS/Exercise1.js
+++ Exercises JavaScript/ExercisesJS/Exercise1.js
@@ -1,22 +1,29 @@
+// 1) Programa una Funcion que dado de Numeros retorne cual es el mayor
+import { validateNumber } from './validations.js';
 "use strict";
 
-do {
-  var Number1 = parseInt(prompt("Digite un numero : ", 0));
-  var Number2 = parseInt(prompt("Digite otro numero : ", 0));
+let Number1 = parseInt(prompt("Digite un numero : ", 0));
+let Number2 = parseInt(prompt("Digite otro numero : ", 0));
 
-  if (Number1 < Number2) {
-    console.log("El numero mayor es : " + Number2);
-    alert("El numero mayor es : " + Number2);
-    document.write("El numero mayor es : " + Number2);
-  } else if (Number2 < Number1) {
-    console.log("El numero mayor es : " + Number1);
-    alert("El numero mayor es : " + Number1);
-    document.write("El numero mayor es : " + Number1);
-  } else if (Number1 == Number2) {
-    console.log("Los numeros introducidos son iguales.");
-    alert("Los numeros introducidos son iguales.");
-    document.write("Los numeros introducidos son iguales.");
+((function(Number1, Number2){
+  
+  if (validateNumber(Number1) && validateNumber(Number2)){
+    if (Number1 < Number2) {
+      console.log("El numero mayor es : " + Number2);
+      alert("El numero mayor es : " + Number2);
+      document.write("El numero mayor es : " + Number2);
+    } else if (Number2 < Number1) {
+      console.log("El numero mayor es : " + Number1);
+      alert("El numero mayor es : " + Number1);
+      document.write("El numero mayor es : " + Number1);
+    } else {
+      console.log("Los numeros introducidos son iguales.");
+      alert("Los numeros introducidos son iguales.");
+      document.write("Los numeros introducidos son iguales.");
+    } 
   } else {
     alert("Introduza Numeros No Simbolos ni Caracteres");
   }
-} while (isNaN(Number1) || isNaN(Number2));
+
+})(Number1, Number2));
+  
