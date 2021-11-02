d197d6ca075f6271bcfae99bd36238c2bd8aba08
Zelechos <zelechos@hotmail.com>
2021-11-01 22:39:19 -0400

    v0.2.0

--- Exercises JavaScript/ExercisesJS/Exercise3.js
+++ Exercises JavaScript/ExercisesJS/Exercise3.js
@@ -1,16 +1,15 @@
-'use strict'
+//3) Programa una Funcion que introduzca un limite inferior y un limite superior rellene con numeros la sucesion ejemplo function(2,10) => 2,3,4,5,6,7,8,9,10
+((function (){
+    let LimiteInferior = parseInt(prompt("Digite Limite Inferior de los numeros que se mostraran : ",0));
+    let LimiteSuperior = parseInt(prompt("Digite Limite Superior de los numeros que se mostraran : ",0));
 
-
-do{
-var LimiteInferior = parseInt(prompt("Digite Limite Inferior de los numeros que se mostraran : ",0));
-var LimiteSuperior = parseInt(prompt("Digite Limite Superior de los numeros que se mostraran : ",0));
-
-if(isNaN(LimiteInferior) || isNaN(LimiteSuperior)){
-  alert("Digite Numeros Porfa");
-}else{
-  document.write("<h1 align = center><strong><em>Los numeros de "+LimiteInferior+" hasta "+LimiteSuperior+" son :</em></strong></h1><br>");
-  for(var i = LimiteInferior; i <= LimiteSuperior;i++){
-  document.write("["+i+"]");
-}
-}
-}while(isNaN(LimiteInferior) || isNaN(LimiteSuperior));
+    if(isNaN(LimiteInferior) || isNaN(LimiteSuperior)){
+      console.error(`was expected numbers not => ${typeof LimiteInferior} and ${typeof LimiteSuperior}`);
+    }else{
+      let numbers = [];
+      for(let index = LimiteInferior; index <= LimiteSuperior; index++){
+        numbers.push(index);
+      }
+      console.log(numbers);
+    }
+})());
--- Exercises JavaScript/ExercisesJS/Exercise4.js
+++ Exercises JavaScript/ExercisesJS/Exercise4.js
@@ -1,16 +1,19 @@
+// 4) Programa una Funcion que introduzca un limite inferior y un limite superior devuelva una sucesion de numeros impares ejemplo function(1,10) => 1,3,5,7,9
 'use strict'
 
-do{
-var LimiteInferior = parseInt(prompt("Introduzca Su Limite Inferior",0));
-var LimiteSuperior = parseInt(prompt("Introduzca Su Limite Superior",0));
-if (isNaN(LimiteInferior) || isNaN(LimiteSuperior)) {
-  alert("No introdujo Numero porfa Hagalo");
-}else{
-  for (var Numero = LimiteInferior; Numero <= LimiteSuperior ; Numero++) {
-    if(Numero%2 != 0){
-      console.log(Numero);
-      document.write("<strong>["+Numero+"]</strong>");
+const sequenceNumbersPairs = () =>{
+  let LimiteInferior = parseInt(prompt("Introduzca Su Limite Inferior",0));
+  let LimiteSuperior = parseInt(prompt("Introduzca Su Limite Superior",0));
+  if (isNaN(LimiteInferior) || isNaN(LimiteSuperior)) {
+    console.error("No introdujo Numero porfa Hagalo");
+  }else{
+    let odds = [];
+    for (let number = LimiteInferior; number <= LimiteSuperior ; number++) {
+      if(number % 2 != 0){
+        odds.push(number);
+      }
     }
+    console.log(odds);
   }
 }
-}while(isNaN(LimiteInferior) || isNaN(LimiteSuperior));
+sequenceNumbersPairs();
