ea8e9db1c915f49da1e1de91f27016b63e050926
Zelechos <zelechos@hotmail.com>
2021-11-08 00:17:40 -0400

    v0.2.1

--- Exercises JavaScript/ExercisesJS/Exercise43.js
+++ Exercises JavaScript/ExercisesJS/Exercise43.js
@@ -1,22 +1,38 @@
 // 43) desarrollar una funcion que dado que pida un numero si el numero es impar el programa finalize si el numero es par mostrar los numeros en pantalla ejemplo function(5) programa finalizo
+window.addEventListener('load', ()=> {
+    'use strict'
 
-let number = Number(prompt("enter a number : ",0));
-const pairToContinue = number =>{
-    
-    if(number % 2 ===0){
-        setInterval(() => {
-            number = Number(prompt("enter a number : ",0));
-            pairToContinue(number);
-        }, interval);
-    }else{
+    let number = 0;
+    let recorderPairs = [];
 
+    const pairToContinue = number =>{
+        let timeout = 1000;
+        let execution = setTimeout(() => {
+            number = Number(prompt("enter a number pair : ",0));
+            if(number % 2 ===0){
+                record(number);
+                pairToContinue(number);
+            }else{
+                clearTimeout(execution);
+                document.write(`<h1>ADIOS</h1>`);
+            }
+        }, timeout);
     }
-    
 
-}
+    const record = number =>{
+        let response = "";
+        recorderPairs.push(number);
+        for (const element of recorderPairs) {
+            response += "["+element+"]=>";
+        }
+        document.write(
+            `<ul>
+                <li><h1>${response}</h1></li>
+            </ul>
+            `
+        );
+            
+    }
 
-const record = number =>{
-    let recorderPairs
-}
-
-
+    pairToContinue(number);
+});
