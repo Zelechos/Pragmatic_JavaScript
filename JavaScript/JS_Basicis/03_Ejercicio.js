763e88e9b832b80e546344dc104aec98f537c9af
Zelechos <zelechos@hotmail.com>
2021-11-14 20:43:52 -0400

    v0.2.2

--- JavaScript/JS_Basicis/03_Ejercicio.js
+++ JavaScript/JS_Basicis/03_Ejercicio.js
@@ -1,28 +1,50 @@
-'use strict'
+// Crear un funcindexon que devuelva la tabla de multindexplindexcar de un number escogindexdo!!
+window.addEventListener('load', ()=> {
 
-var Number = parseInt(prompt("De que Numero desea saber la tabla de Multiplicar: "));
-var Numero = parseInt(prompt("Hasta que numero deseas Conocer las Tablas de Multiplicacion?"));
+    'use strict'
 
-document.write("<h1>La Tabla de Multiplicar de "+Number+"</h1>");
-for (var i = 1 ; i <= 10 ; i++){
-    document.write(i+" x "+Number+" = "+Number*i+"<br>");
-}
+    // funcion para obtener la tabla de un numero especifico
+    const numberTable = () => {
+        let number = parseInt(prompt("De que number desea saber la tabla de Multindexplindexcar: "));
 
-document.write("<h1>Todas las Tablas de Multiplicar</h1>");
-document.write("<table border = 1>");
-document.write("<tr>");
-    for (let k = 1; k <= Numero ; k++) {
-    document.write("<th>Tabla del "+k+"</th>");    
+        document.write("<h1>La Tabla de Multiplicar de "+number+"</h1>");
+        document.write(`<ul>`);
+        for (let index = 1 ; index <= 10 ; index++){
+            document.write(`<li> ${index} x ${number} = ${number*index} </li>`);
+        }
+        document.write(`</ul>`);
     }
-document.write("</tr>");
 
-for (let j = 1; j <= Numero ; j++) {
-    document.write("<tr>");
-        for (var i = 1 ; i <= Numero ; i++){
-            document.write("<td>");
-            document.write(i+" x "+j+" = "+j*i+"<br>");
-            document.write("</td>");
-        }   
-    document.write("</tr>");
-}
-document.write("</table>");
+    // funcion para obtener las tablas de todos los numeros anteriores al introducido
+    const multiplicationTables = ()=>{
+        let number = parseInt(prompt("numero para obetener la Tablas de Multiplicacion ?"));
+        
+        document.write("<h1>Todas las Tablas de Multiplicarcar</h1>");
+        document.write("<table border = 1>");
+
+        document.write("<tr>");
+        for (let index = 1; index <= number ; index++) {
+            document.write("<th>Tabla del "+index+"</th>");    
+        }
+        document.write("</tr>");
+        
+        for (let bigIndex = 1; bigIndex <= 10 ; bigIndex++) {
+            document.write("<tr>");
+            for (var index = 1 ; index <= number ; index++){
+                document.write("<td>");
+                document.write(index+" x "+bigIndex+" = "+index*bigIndex+"<br>");
+                document.write("</td>");
+            }   
+            document.write("</tr>");
+        }
+        document.write("</table>");
+    }
+
+    ((function(){
+
+            document.getElementById('body').innerHTML = '';
+            numberTable();
+            multiplicationTables();
+    })());
+    
+});
--- JavaScript/JS_Basicis/04_Ejercicio.js
+++ JavaScript/JS_Basicis/04_Ejercicio.js
@@ -1,33 +1,38 @@
-'use strict'
-do{
-    var Number1 = parseInt(prompt('Digite el Primer Numero : ',0));
-    var Number2 = parseInt(prompt('Digite el Segundo Numero : ',0));
+// crear una funcion para sumar, restar, multiplicacion, division que calcule en base dos numero introducidos
+window.addEventListener('load', ()=> {
 
-    if(isNaN(Number1) || isNaN(Number2)){
-        alert("No Introdujo un Numero en Cualquiera de las Dos Oportunidades para Hacerlo : ");
-    }else{
-        var Suma = Number1 + Number2;
-        var Resta = Number1 - Number2;
-        var Multiplicacion = Number1 * Number2;
-        var Division = Number1 / Number2;
+    'use strict'
+    ((function(){
+        document.getElementById('body').innerHTML = '';
 
-        document.write("<h1>Las Respuestas de Las Operaciones son :</h1>");
+        do{
+            let number1 = parseInt(prompt('Digite el Primer Numero : ',0));
+            let number2 = parseInt(prompt('Digite el Segundo Numero : ',0));
+            
+            if(isNaN(number1) || isNaN(number2)){
+                alert("No Introdujo un Numero en Cualquiera de las Dos Oportunidades para Hacerlo : ");
+            }else{
+                let suma = number1 + number2;
+                let resta = number1 - number2;
+                let multiplicacion = number1 * number2;
+                let division = number1 / number2;
+                
+                document.write("<h1>Las respuestas de Las Operaciones son :</h1>");
+                
+                let respuesta ="La suma es: "+(suma)+"<br>"+
+                "La resta es: "+(resta)+"<br>"+
+                "La multiplicacion es: "+(multiplicacion)+"<br>"+
+                "La division es: "+(division)+"<br>";
+                
+                let respuesta1 ="La suma es: "+(suma)+"\n"+
+                "La resta es: "+(resta)+"\n"+
+                "La multiplicacion es: "+(multiplicacion)+"\n"+
+                "La division es: "+(division)+"\n";
+                document.write(respuesta);
+                console.log(respuesta1);
+                alert(respuesta1);
+            }
+        }while(isNaN(number1) || isNaN(number2));
         
-        var Respuesta ="La Suma es: "+(Suma)+"<br>"+
-                        "La Resta es: "+(Resta)+"<br>"+
-                        "La Multiplicacion es: "+(Multiplicacion)+"<br>"+
-                        "La Division es: "+(Division)+"<br>";
-
-        var Respuesta1 ="La Suma es: "+(Suma)+"\n"+
-                        "La Resta es: "+(Resta)+"\n"+
-                        "La Multiplicacion es: "+(Multiplicacion)+"\n"+
-                        "La Division es: "+(Division)+"\n";
-        document.write(Respuesta);
-        console.log(Respuesta1);
-        alert(Respuesta1);
-    }
-
-
-
-}while(isNaN(Number1) || isNaN(Number2));
-
+    })());
+});
