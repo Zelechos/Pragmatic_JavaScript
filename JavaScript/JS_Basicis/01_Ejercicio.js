4394266a48b172fb5fc67aafb4433df8f7d38cd9
Zelechos <zelechos@hotmail.com>
2021-11-14 20:04:06 -0400

    v0.2.2

--- JavaScript/JS_Basicis/01_Ejercicio.js
+++ JavaScript/JS_Basicis/01_Ejercicio.js
@@ -1,9 +1,9 @@
-'use strict'
-let number = parseInt(prompt("Digite un Numero : "));
-
-for(let divider = 1; divider <= number; divider++) {
-
-    if (number % divider == 0) {
-        console.log("Divisor es : "+divider);
+// 1) Crear una funcion que obtenga los divisores del numero introducido por teclado function(1) devuelve 1
+((function(){
+    let number = parseInt(prompt("enter a number : "));
+    for(let divider = 1; divider <= number; divider++) {
+        if (number % divider == 0) {
+            console.log(`the divider of ${number} is : ${divider}`);
+        }
     }
-}
+})());
--- JavaScript/JS_Basicis/02_Ejercicio.js
+++ JavaScript/JS_Basicis/02_Ejercicio.js
@@ -1,21 +1,19 @@
+// 2) crear una function que diga si el numero introducido por teclado es par o impar function(4) el numero es PAR
 'use strict'
 
-do{
-var Number = parseInt(prompt("Digite un Numero : "));
+const parImpar = Number => (Number % 2 == 0)? console.log(Number,' es Par') : console.log(Number,' es Impar');
+let number = 0;
+((function(){
 
-    if (isNaN(Number)){
-
-        alert("No es un Numero");
-
-    }else{
-
-        if (Number % 2 == 0){
-            document.write("El Numero es Par <br>");
+    do{
+        number = parseInt(prompt("enter a number : "));
+        if (isNaN(number)){
+            alert("is not a number");
+            console.error("is not a number");
         }else{
-            document.write("El Numero es Impar <br>");
+            parImpar(number);
         }
 
-    }
-}while(isNaN(Number));
-
-document.write("El Numero es : "+Number);
+    }while(!isNaN(number));
+    console.warn("code finish!!!");
+})());
