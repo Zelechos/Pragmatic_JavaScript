415a06a059e0f84015aec27a11c257fc28f46d4a
Zelechos <zelechos@hotmail.com>
2021-11-16 22:49:12 -0400

    v0.2.2

--- Exercises JavaScript/ExercisesJS/Exercise9.js
+++ Exercises JavaScript/ExercisesJS/Exercise9.js
@@ -1,30 +1,26 @@
+//9) Hacer un Programa Con Pruebas de error try-catch Que dado un Numero Obtenga el Factorial
 'use strict'
-// Hacer un Programa Con Pruebas de error Que dado un Numero Obtenga el Factorial
 window.addEventListener('load', ()=>{
+
     let etiqueta = document.getElementById("bod");
     etiqueta.style.backgroundColor = "black";
     etiqueta.style.color = "white";
 });
 
 let number = prompt("Digite Un Numero para hallar su Factorial : ");
-let Resultado = 0;
+let response = 0;
 
 try {
     if(isNaN(number)){
         throw new Error("El dato introducido No es un Numero!!!!")
     }
-    Resultado = Factorial(number);
+    response = factorial(number);
 } catch (error) {
     console.error("Algo Salio mal ==> "+ error);
 }finally{
-    document.write(`<h1>El  Factorial de ${number} es ${Resultado}</h1>`);
+    document.write(`<h1>El  Factorial de ${number} es ${response}</h1>`);
     console.warn("Termino el Proceso");
 }
 
 // Funcion para Hayar el Factorial
-function Factorial(Numero){
-    if(Numero== 0 || Numero ==1){
-        return 1;
-    }
-    return Numero * Factorial(Numero-1);
-}
+const factorial = number => (number== 0 || number ==1)? 1 : number * factorial(number-1);
