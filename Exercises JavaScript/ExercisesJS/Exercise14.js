bab5862d5706534a1402eba92f518740f0e0d2fd
Zelechos <zelechos@hotmail.com>
2021-11-16 23:07:54 -0400

    v0.2.2

--- Exercises JavaScript/ExercisesJS/Exercise14.js
+++ Exercises JavaScript/ExercisesJS/Exercise14.js
@@ -1,51 +1,52 @@
+// 14) Crear un programa que dado unos datos muestre y guarde los datos introducidos dados como un historial de intentos
 'use strict'
 
 window.addEventListener('load', ()=>{
 
-function main(){
-    let answer = confirm("Eres un Hacker?");
-    
-    if(answer){
-        const nickname = prompt("Cual es su NickName : ");
-        greet(nickname);
+    function main(){
+        let answer = confirm("Eres un Hacker?");
         
-        const darkWeb = prompt("Que has hecho en la Dark Web");
-        answerdark(darkWeb);
-    }else{
-        document.write(`<p id="text-loser">&lt;you are useless!!! loser&gt;</p>` );
+        if(answer){
+            const nickname = prompt("Cual es su NickName : ");
+            greet(nickname);
+            
+            const darkWeb = prompt("Que has hecho en la Dark Web");
+            answerdark(darkWeb);
+        }else{
+            document.write(`<p id="text-loser">&lt;you are useless!!! loser&gt;</p>` );
 
-        const text = document.querySelector("#text-loser");
-        text.style.cursor="pointer";
-        text.style.fontSize = "50px";
-        
-        let pixels = 100;
-        text.addEventListener("click",()=>{
-            text.style.fontSize = pixels+"px";
-            pixels+=10;
-        });
+            const text = document.querySelector("#text-loser");
+            text.style.cursor="pointer";
+            text.style.fontSize = "50px";
+            
+            let pixels = 100;
+            text.addEventListener("click",()=>{
+                text.style.fontSize = pixels+"px";
+                pixels+=10;
+            });
+        }
     }
-}
 
-function greet (nickname){
-    nickname = nickname || "nobody";
-    document.write(`
-    <fieldset>
-        <legend>Hack</legend>
-        <p>Hello ${nickname}</p>
-    </fieldset>
-    `);
-}
+    function greet (nickname){
+        nickname = nickname || "nobody";
+        document.write(`
+        <fieldset>
+            <legend>Hack</legend>
+            <p>Hello ${nickname}</p>
+        </fieldset>
+        `);
+    }
 
-function answerdark(content){
-    content = content || "you are nobody you don't have hacker skills you don't have power";
-    document.write(`
-    <fieldset>
-        <legend>Hack</legend>
-        <p>${content}</p>
-    </fieldset>
-    `);
-}
+    function answerdark(content){
+        content = content || "you are nobody you don't have hacker skills you don't have power";
+        document.write(`
+        <fieldset>
+            <legend>Hack</legend>
+            <p>${content}</p>
+        </fieldset>
+        `);
+    }
 
-main();
+    main();
 
 });
