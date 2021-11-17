a3cc19be45800970475f3d4aa7448cd382181e8d
Zelechos <zelechos@hotmail.com>
2021-11-17 17:30:12 -0400

    v0.2.3

--- JavaScript/JS_FuncionesFlecha/01_FuncionesFlecha.js
+++ JavaScript/JS_FuncionesFlecha/01_FuncionesFlecha.js
@@ -1,17 +1,23 @@
 'use strict'
+// callbacks
 
-/* Ejemplo
-function ShowAlert(){
-    alert("Acaso me LLamaste");
-}
-button.addEventListener('Botoncito',ShowAlert);
-*/
+// Ejemplo 1
 
-function Bienvenida(Callback){
+function welcome(callback){
     document.write("<h2>Funciones Flecha & Callbacks</h2>");
     document.write("<h3>Curso JavaScript</h3>");
     document.write("<blockquote>Y Falta Mucho Mas</blockquote>");
-    Callback();
+    callback();
 }
 
-Bienvenida(function(){console.log("Fin de la Funcion Bienvenida . . . ")})
+welcome(function(){console.log("Fin de la Funcion Bienvenida . . . ")});
+
+// Ejemplo 2
+const hour = (message, callback) =>{
+    console.log(`good ${message} `);
+    callback(new Date());
+}
+
+hour("night", (date)=>{
+    console.log(`the hour is : ${date.getHours()}:${date.getMinutes()}`);
+});
---
+++ Vanilla JavaScript/Asincronia/03_Callbacks.js
@@ -0,0 +1,34 @@
+'use strict'
+// Callbacks
+// NOTA: el callback es un mecanismo que tenemos para manejar el asincronia
+function squareCallback (value, callback, timeout){
+    let numberAleatorio = Math.round(Math.random() * 1000);//0 al 1000
+    setTimeout(() => {
+        callback(value, value*value);
+    },  timeout || numberAleatorio);
+}
+
+// Aqui creamos nuestro primer callback
+squareCallback(0, (value, result)=> {
+    console.log("Beginning Callback!!");
+    console.log(`Callaback : value=> ${value}, result=> ${result} .`);
+
+    // Aqui iniciamos el callback hell!!
+    squareCallback(1, (value, result)=> {
+        console.log(`Callaback : value=> ${value}, result=> ${result} .`);
+
+        squareCallback(2, (value, result)=> {
+            console.log(`Callaback : value=> ${value}, result=> ${result} .`);
+
+            squareCallback(3, (value, result)=> {
+                console.log(`Callaback : value=> ${value}, result=> ${result} .`);
+
+                squareCallback(4, (value, result)=> {
+                    console.log(`Callaback : value=> ${value}, result=> ${result} .`);
+                    console.warn(`End to callback hell !!! `);
+                });
+                // Esto puede ser infinito si queremos....
+            });
+        });
+    });
+});
