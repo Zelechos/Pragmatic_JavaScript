5f9e83566df01c21a82247f70c2a41b9a5d839cf
Zelechos <zelechos@hotmail.com>
2021-11-16 22:37:12 -0400

    v0.2.2

--- Exercises JavaScript/ExercisesJS/FACTORIAL.js
+++ Exercises JavaScript/ExercisesJS/FACTORIAL.js
@@ -1,14 +1,5 @@
+// funcion que sirve para hallar el factorial de un numero
 'use strict'
 
-// let Numero = parseInt(prompt("Digite un Numero Para Hayar Su Factorial : ",0));
-
-export default function factorial(Numero){
-  if(Numero== 0 || Numero ==1){
-    // console.log(Numero);
-    return 1;
-  }
-  return Numero * factorial(Numero-1);
-}
-
-// document.write("<h1> EL factorial es : "+factorial(Numero)+"</h1>");
-//console.log("EL Factorial es : "+Factorial(Numero))
+const factorial = number => (number== 0 || number ==1)? 1 : number * factorial(number-1);
+export default factorial();
