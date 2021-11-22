2470ad418a372b6cdce06f5907afa152824bb4ed
Zelechos <zelechos@hotmail.com>
2021-11-22 19:11:13 -0400

    v0.2.3

--- Exercises JavaScript/ExercisesJS/FACTORIAL.js
+++ Exercises JavaScript/ExercisesJS/FACTORIAL.js
@@ -1,5 +1,5 @@
 // funcion que sirve para hallar el factorial de un numero
 'use strict'
 
-const factorial = number => (number== 0 || number ==1)? 1 : number * factorial(number-1);
+const factorial = (number = 0) => (number === 0 || number === 1)? 1 : number * factorial(number - 1);
 export default factorial();
