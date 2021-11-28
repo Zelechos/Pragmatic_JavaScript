b80df3cbbd74359341de7a12e8c846747fc5e268
Zelechos <zelechos@hotmail.com>
2021-11-28 02:10:12 -0400

    v0.2.4

---
+++ Exercises JavaScript/ExercisesJS/Exercise46.js
@@ -0,0 +1,4 @@
+// 45) crear una funcion de tipo Promesa que consuma un API que encripte un mensaje y luego lo muestre el mensaje junto con el mensaje encriptado en el arichivo HTML de la web usando async await!!!
+'use strict';
+
+const 
