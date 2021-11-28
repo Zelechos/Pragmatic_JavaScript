72d13630b48288138fbcf84439ed3fb598793085
Zelechos <zelechos@hotmail.com>
2021-11-27 22:30:13 -0400

    v0.2.3

--- JavaScript/JS_Arrays/01_Arrays.js
+++ JavaScript/JS_Arrays/01_Arrays.js
@@ -1,52 +1,97 @@
 'use strict'
 
 //Manera 1 de crear un Array
+const movies = ['Dark', 'Rango','Ether'];
 
-var peliculas = ['Dark', 'Rango','Ether'];
+//Manera 2 de Crear un Array
+const lenguages = new Array('HTML', 'CSS', 'JavaScript', 'TypeScript', 'Node Js','React Js', 'Three Js', 'Git');
 
-//Manera Numero 2 de Crear un Array
+console.log(movies);
+console.log(lenguages);
 
-var Lenguajes = new Array('HTML', 'CSS', 'JavaScript' , 'Node Js' ,'React Js' , 'Git');
+const renderStyles = () => {
 
-console.log(peliculas);
-console.log(Lenguajes);
+    document.write(`
+        <style>
 
+            body{
+                background-color : #0d1117;
+                color: #1f6feb;
+            }
 
-var indice = parseInt(prompt('Que Leguaje Quiere aprender : ',0));
-
-if (indice >= Lenguajes.length){
-    alert('El Elemento no Existe introduce un numero menor que '+Lenguajes.length);
-}else{
-    alert(Lenguajes[indice]);    
+            h1{
+                border : 3px solid #8b949E;
+                border-radius : 10px;
+                padding : 10px;
+            }
+        
+        </style>
+    `);
 }
 
-document.write("<h1>Esto es Lo que Hay que Aprender para Trabajar y Ganar Plata</h1>");
-document.write("<ul>");
-for(var I = 0 ; I < Lenguajes.length; I++ ){
-        document.write("<li>"+Lenguajes[I]+"</li>");
+renderStyles();
+
+const selectLenguage = () => {
+
+    let index = parseInt(prompt(`what programmer lenguage do you want? : `,0));
+    
+    if (index >= lenguages.length){
+        alert(`Element does not exist enter a number less than : ${lenguages.length}`);
+    }else{
+        alert(lenguages[index]);    
     }
-    document.write("</ul>");
+}
+
+selectLenguage();
+
+
+/*--------------------------- BUCLE FOR GENERICO IMPORTANTISIMO-------------------*/
+
+const renderLenguagesForGeneric = () => {
+    
+    document.write(`
+    <h1 style="background:#A371F7;">Show with loop FOR GENERIC</h1>
+    <ul>`);
+    for(let technology = 0 ; technology < lenguages.length; technology++ ){
+        document.write(`<li>${lenguages[technology]}</li>`);
+    }
+    document.write(`</ul>`);
+}
+
+renderLenguagesForGeneric()
 
 /*--------------------------- BUCLE FOR EACH IMPORTANTISIMO-------------------*/
 
-document.write("<h1 style='background:green;'>Esta Lista LLega Gracias A El BUCLE FOR EACH</h1>");
+const renderLenguagesForEach = () => {
+    document.write(`
+        <h1 style="background:#26a641;">Show with loop FOR EACH</h1>
+            <ul>
+    `);
+        lenguages.forEach(//Este forEach() trabaja con un Callback
+            (element , index , array)=>{
+                console.log(array);
+                document.write(`<li>${index} - ${element} </li>`);
+            }
+        );
+    document.write(`</ul>`);
+} 
 
-document.write("<ul>");
-    Lenguajes.forEach(//Este forEach() trabaja con un Callback
-        (Elemento , index , array)=>{
-            console.log(array);
-            document.write("<li style= background:red ;>"+index+"-"+Elemento+"</li>");
-        }
-    );
-document.write("</ul>");
+renderLenguagesForEach();
 
 /*--------------------------- BUCLE FOR IN IMPORTANTISIMO-------------------*/
 
-document.write("<h1 style='background:blue;'>Esta Lista LLega Gracias A El BUCLE FOR IN</h1>");
-document.write("<ul>");
-//en teoria la variable que declaramos en Leguajes en esta caso Lenguaje
-//recorre desde 0 la longitud del array que le pongamos 
-        for(let Lenguaje in Lenguajes){//Donde Lenguaje es el Indice
-            document.write("<li style= background:gray ;>"+Lenguaje+"-"+Lenguajes[Lenguaje]+"</li>");
-        }
-document.write("</ul>");
+const renderLenguagesForIn = () => {
+
+    document.write(`
+    <h1 style='background:#58a6ff;'>Show with loop FOR IN</h1>
+        <ul>
+    `);
+    //en teoria la variable que declaramos en Leguajes en esta caso Lenguaje
+    //recorre desde 0 la longitud del array que le pongamos 
+    for(let lenguage in lenguages){//Donde Lenguaje es el index
+        document.write(`<li>${lenguage} - ${lenguages[lenguage]}</li>`);
+    }
+    document.write(`</ul>`);
+}
+
+renderLenguagesForIn();
