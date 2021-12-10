4a0a9eca8eeb25287dc600183d459b2fc0d42cc9
Pragmatic Coder <zelechos@hotmail.com>
2021-12-10 19:09:24 -0400

    v0.2.4

---
+++ Vanilla JavaScript/Nuevos_Tipos/06_Generators.js
@@ -0,0 +1,65 @@
+'use strict'
+
+// Funcion de Tipo Generator
+function* generator(){
+    yield "hey etherias";
+    console.log(`are you not tired to hack?`);
+    yield "no.. i love hack";
+    console.error(`so can you hack mi boss??`);
+    yield "no thanks";
+    yield "bye...";
+}
+
+let iterator = generator();
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+
+for (const message of iterator) {
+    console.log(message);
+}
+
+const messages = [...generator()];
+console.log(messages);
+
+console.warn(`--------------- function generator square --------------------`);
+
+// ejemplo aplicando otra Funcion Generator
+const square = value => {
+    // create a object
+    let numberToSquare = {
+        value, 
+        square: Math.pow(value, 2)
+    };
+
+    setTimeout(()=>{
+        return console.log(numberToSquare);
+    },3000);
+
+}
+
+// funcion generator
+function* generatorSquare(){
+    console.warn(`Begin Generator Function`);
+    yield square(9);
+    yield square(2);
+    yield square(3);
+    yield square(10);
+    console.warn(`End Generator Function`);
+}
+
+let cloneGenerator = generatorSquare();
+
+for(const response of cloneGenerator){
+    console.log(response);
+}
+
+
+
+
+
+
+
+
+
