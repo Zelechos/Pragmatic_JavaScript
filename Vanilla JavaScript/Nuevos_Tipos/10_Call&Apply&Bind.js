2c16e600bd68df5b5b47861521c8ea875d208b56
Pragmatic Coder <zelechos@hotmail.com>
2021-12-24 18:06:42 -0400

    v0.2.5

--- Vanilla JavaScript/Nuevos_Tipos/10_Call&Apply&Bind.js
+++ Vanilla JavaScript/Nuevos_Tipos/10_Call&Apply&Bind.js
@@ -1,3 +1,55 @@
 'use strict'
 
 // Call , Apply , Bind
+console.log(this);
+
+this.lugar = "Global Context";
+
+function greet(context){
+    console.log(`${context}`);
+}
+
+function greetContext(greet, name){
+    console.log(`${greet} ${name} desde el ${this.lugar}`);
+}
+
+greet(this.lugar);
+
+// Creamos un objeto para probar en contexto se encuentra
+
+const object = {
+    lugar : "Object Context",
+}
+
+// con el metodo call(); LLamamos al this de un objeto 
+greetContext.call(object, "Hola", "Alex");
+
+// si le pasamo como parametro al metodo call(); this nos manda al contexto global
+greetContext.call(this, "Hola", "Alex");
+
+
+// tambien con el metodo apply(); llamamos tambien al this del objeto que metemos como parametros
+greetContext.apply(object, ["Adios", "Coder"]);
+
+// si mandamos como parametros this al metodo apply() pasa los mismo
+greetContext.apply(this, ["Adios", "Coder"]);
+
+
+this.name= "Window";
+
+// Creamos otro objeto
+const person = {
+    name: "Alex",
+    greet: function(){
+        console.log(`Hello ${this.name}`);
+    }
+}
+
+person.greet();
+
+// ahora vamos a crear otro objeto para enlazar el contexto con el metodo bind();
+const monster = {
+    greet: person.greet.bind(person)
+}
+
+monster.greet();
