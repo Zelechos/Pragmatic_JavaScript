090f97a02c7b57a97eefee4479cbb1e62c4a4388
Zelechos <zelechos@hotmail.com>
2021-12-26 07:29:35 -0400

    v0.2.6

--- JavaScript/JS_Arrays/02_Arrays.js
+++ JavaScript/JS_Arrays/02_Arrays.js
@@ -1,12 +1,12 @@
 'use strict'
 
-var Generos = ['Terror','Comedia','Romance'];
-var Peliculas = ['Gray','100 days','Argo'];
+let Generos = ['Terror','Comedia','Romance'];
+let Peliculas = ['Gray','100 days','Argo'];
 
-var Cine = [Generos , Peliculas];
+let Cine = [Generos , Peliculas];
 
 do{
-    var Elemento = prompt('Digite una Pelicula : ');
+    let Elemento = prompt('Digite una Pelicula : ');
     if(Elemento != 'x'){
         Peliculas.push(Elemento);
     }
@@ -14,7 +14,7 @@ do{
 
 console.log(Peliculas);
 
-var indice = Peliculas.indexOf('100 days');
+let indice = Peliculas.indexOf('100 days');
 if (indice > -1){
     Peliculas.splice(indice,1);
 }
@@ -22,7 +22,7 @@ if (indice > -1){
 console.log(Peliculas);
 
 console.log(Peliculas.join());//Para convertir un array a texto
-var pel =Peliculas.join();
+let pel =Peliculas.join();
 console.log(pel.split(","));//Para convertir un texto a array
 
 //Ordenando el Arreglo
@@ -33,17 +33,17 @@ console.log(Generos);
 console.log("El Arreglo Invertido es : "+Generos.reverse());
 
 /*-----------------------BUSQUEDAS------------------------------------------*/
-var Busqueda = String(prompt("Que Genero de Pelicula desea? "));
+let Busqueda = String(prompt("Que Genero de Pelicula desea? "));
 
-var Encontrar = Generos.find(Genero=>Genero === Busqueda);
-var Indice = Generos.findIndex(Genero=>Genero === Busqueda);
+let Encontrar = Generos.find(Genero=>Genero === Busqueda);
+let Indice = Generos.findIndex(Genero=>Genero === Busqueda);
 
 console.log("Genero Encontrado : "+Indice+" .- "+Encontrar);
 
-var Precios =[10,50,90,150,200,450,750,900,1250,1500];
-var Economico = Precios.some(Precio=> Precio >= 90);
-var Estandar = Precios.some(Precio=> Precio >= 750);
-var Calidad = Precios.some(Precio=> Precio >= 1500);
+let Precios =[10,50,90,150,200,450,750,900,1250,1500];
+let Economico = Precios.some(Precio => Precio >= 90);
+let Estandar = Precios.some(Precio => Precio >= 750);
+let Calidad = Precios.some(Precio => Precio >= 1500);
 
 console.log("Pollo Economico Precios Desde :"+Economico);
 console.log("Pollo Estandar Precios Desde :"+Estandar);
