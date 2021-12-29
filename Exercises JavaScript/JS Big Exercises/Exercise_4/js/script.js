72610480aa31748f747c698234da32e18b26d6e8
Zelechos <zelechos@hotmail.com>
2021-12-28 20:22:57 -0400

    v0.2.6

---
+++ Exercises JavaScript/JS Big Exercises/Exercise_4/js/script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+window.addEventListener('load',()=>{
+    
+    copyStyle();
+    generatorDataset();
+    
+});
+
+
+const generatorDataset = ()=>{
+    const styleArea = document.querySelector('#code');
+
+    // Aqui le pasamos la etiqueta queramos
+    styleArea.innerHTML= body(1000);
+}
+
+const generatorColors = () =>{
+    const digitColors = "01234565789ABCDEF";
+    let color = "#";
+    for (let index = 0; index < 6; index++) {
+        color += digitColors.charAt(Math.floor(Math.random() * (15-0)+0));
+    }
+    console.log(color);
+    return color
+}
+
+//Subrutina para Copiar el estilo CSS
+const copyStyle = () => {
+    const button = document.querySelector('#btnCopy');
+    const alert = document.querySelector("#aviso");
+    const input = document.querySelector('#code');
+    
+    button.addEventListener('click',()=>{
+        input.focus();
+        document.execCommand('selectAll');
+        document.execCommand('copy');
+        alert.innerHTML = "Codigo Copiado";
+        setTimeout(()=> alert.innerHTML = "" , 4000);
+    });
+}
+
+// --------------------------------------------------------------------------
+//                          ETIQUETAS   
+// --------------------------------------------------------------------------
+
+// etiqueta body
+const body = labels =>{
+    let bodyLabel = ``;
+    for (let index = 0; index < labels; index++) {
+        let randomFont = Math.floor(Math.random() * (300-250)+250)
+        let randomFontSize = Math.floor(Math.random() * (25-15)+15)
+        bodyLabel += `
+        { 
+        "style" : "body { font-family: 'IBM Plex Sans', sans-serif ; font-weight:${randomFont} ; background-color:${generatorColors()} ; font-size:${randomFontSize}px ; }",
+        "selector" : ".body"
+      } ,`;
+    }
+
+    return bodyLabel;
+}
