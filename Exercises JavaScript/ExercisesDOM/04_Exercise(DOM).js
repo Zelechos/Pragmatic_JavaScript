e755133e9deea9ea24f2f6b0ebdcce284ca66da7
Pragmatic Coder <zelechos@hotmail.com>
2022-01-03 19:39:51 -0400

    v0.2.6

--- Exercises JavaScript/ExercisesDOM/04_Exercise(DOM).js
+++ Exercises JavaScript/ExercisesDOM/04_Exercise(DOM).js
@@ -1,33 +1,14 @@
 'use strict'
 
+const $labelSubtitles = document.querySelectorAll('h4');
 
-var H4S = document.getElementsByTagName('h4');
+const showColor = label => {
+    label.forEach(element =>{
+        let color = element.style;
+        (element.getAttribute("class") === "celeste")
+            ? color.background = "#7792af"
+            : color.background = "#ede3e7";
+    });
+} 
 
-document.write("<ul>");
-//H4S.forEach((element ,index) => { NO SE PUEDE USAR FOREACH PARA ARRAY DE ETIQUETAS
-    //document.write("<li> "+element[index]+"</li>");
-var element;
-for(element in H4S){
-    
-    if( typeof H4S[element].textContent == 'string'){
-        var parrafo = document.createElement("p");
-        var texto = document.createTextNode(H4S[element].textContent);
-        parrafo.appendChild(texto);//tambien Funciona con prepend() y append()
-        document.querySelector("#miseccion").appendChild(parrafo);
-    }
-    
-}
-
-var Celeste = document.getElementsByClassName('celeste');
-
-for(let div in Celeste){
-    if(Celeste[div].className == 'celeste'){
-    Celeste[div].style.background = "#93CEF4";
-    }
-}
-
-var Blanco = document.querySelector(".blanco");
-
-Blanco.style.background = "white";
-console.log(Blanco);
-
+showColor($labelSubtitles);
