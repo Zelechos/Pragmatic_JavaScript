16a72440f46be1b7128486b36cd9738db811e8a8
Zelechos <zelechos@hotmail.com>
2022-01-02 21:05:19 -0400

    v0.2.6

--- Exercises JavaScript/ExercisesDOM/02_Exercise(DOM).js
+++ Exercises JavaScript/ExercisesDOM/02_Exercise(DOM).js
@@ -1,19 +1,22 @@
 'use strict'
 
-function calculateSum() {
+const calculateSum = ()=> {
     const number1 = document.getElementById("X");
     const number2 = document.getElementById("Y");
     let x = parseInt(number1.value);
     let y = parseInt(number2.value);
-    let result = x + y;
+    
+    return x + y;
+}
 
+const renderResponse = ()=>{
     const labelResult = document.getElementById("Resultado");
     
     labelResult.style.fontSize = "300px";
     labelResult.style.textAlign = "center";
     labelResult.style.marginTop = "-250px";
     
-    labelResult.innerHTML  = result;
+    labelResult.innerHTML  = calculateSum();
 }
 
-calculateSum();
+renderResponse();
