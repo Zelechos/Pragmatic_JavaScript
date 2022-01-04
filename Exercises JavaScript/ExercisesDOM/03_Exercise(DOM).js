3162d620e12fdd07f7322bf9ce063075ec4ade29
Pragmatic Coder <zelechos@hotmail.com>
2022-01-03 20:02:09 -0400

    v0.2.6

--- Exercises JavaScript/ExercisesDOM/03_Exercise(DOM).js
+++ Exercises JavaScript/ExercisesDOM/03_Exercise(DOM).js
@@ -3,20 +3,29 @@
 const calculateSum = ()=> {
     const number1 = document.getElementById("X");
     const number2 = document.getElementById("Y");
-    let x = parseInt(number1.value);
-    let y = parseInt(number2.value);
-    
-    return x + y;
+
+    if(isNaN(number1.value) || isNaN(number2.value)){
+        alert("solo se permiten numeros");
+        return "∞";
+    }else{
+        let x = parseInt(number1.value);
+        let y = parseInt(number2.value);
+        return x + y;
+    }
 }
 
 const renderResponse = ()=>{
     const labelResult = document.getElementById("Resultado");
+
+    if(labelResult.innerHTML ===  "response"){
+        labelResult.innerHTML = "";
+    }else{
+        labelResult.style.fontSize = "300px";
+        labelResult.style.textAlign = "center";
+        labelResult.style.marginTop = "-250px";
+        labelResult.innerHTML  = calculateSum();
+    }
     
-    labelResult.style.fontSize = "300px";
-    labelResult.style.textAlign = "center";
-    labelResult.style.marginTop = "-250px";
-    
-    labelResult.innerHTML  = calculateSum();
 }
 
 renderResponse();
