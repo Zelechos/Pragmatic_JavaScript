a30c0f16953383712c36eb5062f79fb630c1a163
Zelechos <zelechos@hotmail.com>
2022-01-30 23:39:40 -0400

    v0.2.6

--- Exercises JavaScript/JS Big Exercises/Exercise_4/js/script.js
+++ Exercises JavaScript/JS Big Exercises/Exercise_4/js/script.js
@@ -12,7 +12,7 @@ const generatorDataset = ()=>{
     const styleArea = document.querySelector('#code');
 
     // Aqui le pasamos la etiqueta queramos
-    styleArea.innerHTML= centerDiv(40);
+    styleArea.innerHTML= imagesHoverRotate(50);
 }
 
 // Subrutina para generar los colores CSS
@@ -374,45 +374,6 @@ const btnTransparent = labels =>{
 }
 
 
-// barra de navegacion vertical
-
-/* <nav class="sidebar-navigation">
-	<ul>
-		<li class="active">
-			<i class="fa fa-share-alt"></i>
-			<span class="tooltip">Connections</span>
-		</li>
-		<li>
-			<i class="fa fa-hdd-o"></i>
-			<span class="tooltip">Devices</span>
-		</li>
-		<li>
-			<i class="fa fa-newspaper-o"></i>
-			<span class="tooltip">Contacts</span>
-		</li>
-		<li>
-			<i class="fa fa-print"></i>
-			<span class="tooltip">Fax</span>
-		</li>
-		<li>
-			<i class="fa fa-sliders"></i>
-			<span class="tooltip">Settings</span>
-		</li>
-	</ul>
-</nav> */
-const navBarVertical = labels =>{
-    let navBar = ``;
-    for (let index = 0; index < labels; index++) {
-        navBar += `
-        { 
-        "style" : "",
-        "selector" : ""
-      } ,`;
-    }
-
-    return navBar;
-}
-
 
 //Centrar un div
 /* 
@@ -435,4 +396,112 @@ const centerDiv = labels =>{
     return div;
 }
 
-// Realizar estilos de h1, h2 , h3 , h4
+// mosaico de imagenes
+/*
+<div class="image-mosaic">
+        <div
+          class="card card-tall card-wide"
+          style="background-image: url('https://picsum.photos/id/564/1200/800')"
+        ></div>
+        <div
+          class="card card-tall"
+          style="background-image: url('https://picsum.photos/id/566/800/530')"
+        ></div>
+        <div
+          class="card"
+          style="background-image: url('https://picsum.photos/id/575/800/530')"
+        ></div>
+        <div
+          class="card"
+          style="background-image: url('https://picsum.photos/id/626/800/530')"
+        ></div>
+        <div
+          class="card"
+          style="background-image: url('https://picsum.photos/id/667/800/530')"
+        ></div>
+        <div
+          class="card"
+          style="background-image: url('https://picsum.photos/id/678/800/530')"
+        ></div>
+        <div
+          class="card card-wide"
+          style="background-image: url('https://picsum.photos/id/695/800/530')"
+        ></div>
+        <div
+          class="card"
+          style="background-image: url('https://picsum.photos/id/683/800/530')"
+        ></div>
+        <div
+          class="card"
+          style="background-image: url('https://picsum.photos/id/693/800/530')"
+        ></div>
+        <div
+          class="card"
+          style="background-image: url('https://picsum.photos/id/715/800/530')"
+        ></div>
+        <div
+          class="card"
+          style="background-image: url('https://picsum.photos/id/610/800/530')"
+        ></div>
+        <div
+          class="card"
+          style="background-image: url('https://picsum.photos/id/599/800/530')"
+        ></div>
+      </div>
+*/
+
+const imagesMosaic = labels =>{
+    let mosaic = ``;
+    for (let index = 0; index < labels; index++) {
+        mosaic += `
+        { 
+        "style" : ".image-mosaic { display:grid ; gap:1rem ; grid-template-columns:repeat(auto-fit,minmax(240px,1fr)) ; grid-auto-rows:240px ; } .card { display:flex ; flex-direction:column ; justify-content:center ; align-items:center ; background:${generatorColors()} ; font-size:3rem ; color:${generatorColors()} ; box-shadow:rgba(3,8,20,.1) 0 .15rem .5rem,rgba(2,8,20,.1) 0 .075rem .175rem ; height:100% ; width:100% ; border-radius:4px ; transition:all 500ms ; overflow:hidden ; background-size:cover ; background-position:center ; background-repeat:no-repeat ; padding:0 ; margin:0 } @media screen and (min-width:600px) { .card-tall { grid-row:span 2/auto ; } .card-wide { grid-column:span 2/auto ; } } ",
+        "selector" : ".image-mosaic"
+      } ,`;
+    }
+
+    return mosaic;
+}
+
+
+// efecto de giro en una imagen
+/*
+<figure class="hover-rotate">
+    <img src="https://picsum.photos/id/669/600/800.jpg"/>
+</figure>
+*/
+
+const imagesRotate = labels =>{
+    let rotate = ``;
+    for (let index = 0; index < labels; index++) {
+        rotate += `
+        { 
+        "style" : ".hover-rotate { overflow:hidden ; margin:8px ; min-width:240px ; max-width:320px ; width:100% ; } .hover-rotate img { transition:all 0.3s ; box-sizing:border-box ; max-width:100% ; } .hover-rotate:hover img { transform:scale(1.3) rotate(5deg) ; } ",
+        "selector" : ".hover-rotate"
+      } ,`;
+    }
+
+    return rotate;
+}
+
+// efecto de giro en una imagen con un diferente selector
+/*
+<figure class="image-rotate">
+    <img src="https://picsum.photos/id/669/600/800.jpg"/>
+</figure>
+*/
+
+const imagesHoverRotate = labels =>{
+    let rotate = ``;
+    for (let index = 0; index < labels; index++) {
+        rotate += `
+        { 
+        "style" : ".image-rotate { overflow:hidden ; margin:8px ; min-width:240px ; max-width:320px ; width:100% ; } .image-rotate img { transition:all 0.3s ; box-sizing:border-box ; max-width:100% ; } .image-rotate:hover img { transform:scale(1.3) rotate(5deg) ; } ",
+        "selector" : ".image-rotate"
+      } ,`;
+    }
+
+    return rotate;
+}
+
+
