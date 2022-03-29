d622798c46cdd8efd42d3361a030fcb7697a5fa5
Pragmatic Coder <zelechos@hotmail.com>
2022-03-29 18:15:54 -0400

    v0.2.8

---
+++ JavaScript/AJAX/05_Axios&AsyncAwait.js
@@ -0,0 +1,44 @@
+(()=>{
+    const $list = document.getElementById('axios-async');
+    const $fragment = document.createDocumentFragment();
+
+    
+    
+    async function getData(){
+        try{
+            // Llamando a la libreria axios!!
+            // let response = await axios.get('./users.json'); ATENCION tambien sirve con archivos locales JSON, etc
+            let response = await axios.get('https://jsonplaceholder.typicode.com/users');
+            let data = await response.data;
+            // console.log(response);
+
+            data.forEach(user => {
+                const $li = document.createElement('li');
+                const $fieldset = document.createElement('fieldset');
+                const $legend = document.createElement('legend');
+                $legend.textContent = `${user.name}`;
+
+                $fieldset.appendChild($legend);
+                $fieldset.innerHTML += `Email : ${user.email} <br> Phone : ${user.phone}`;
+
+                $li.appendChild($fieldset);
+
+                $fragment.appendChild($li);
+            })
+
+            $list.appendChild($fragment);
+            
+        }catch(error){
+            // console.log(error.response);
+            let text = error.response.statusText || "Algo anda mal !!! ";
+            let message = `Error : ${error.response.status} => ${text}`;
+            console.error(message);
+            $list.innerHTML = message;
+
+        }finally{
+            console.warn('Axios with async await');
+        }
+    }
+
+    getData();
+})();
