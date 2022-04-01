36aeaef371f18686db7b86ac99f11800c2d146f7
Pragmatic Coder <zelechos@hotmail.com>
2022-04-01 12:05:21 -0400

    v0.2.8

---
+++ JavaScript/API Rest/API_FETCH/crud_fetch.js
@@ -0,0 +1,14 @@
+
+const d = document;
+
+d.addEventListener('DOMContentLoaded', e=>{
+
+    const $table = d.querySelector('.crud-table');
+    const $form = d.querySelector('.crud-form');
+    const $title = d.querySelector('.crud-title');
+    const $template = d.getElementById('crud-template').content;
+    const $fragment = d.createDocumentFragment();
+
+});
+
+
