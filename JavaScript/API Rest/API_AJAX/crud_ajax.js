7533545d25560544d5237b70e50be739cd9c7a0d
Pragmatic Coder <zelechos@hotmail.com>
2022-04-05 23:59:15 -0400

    v0.2.8

--- JavaScript/API Rest/API_AJAX/crud_ajax.js
+++ JavaScript/API Rest/API_AJAX/crud_ajax.js
@@ -1,13 +1,9 @@
-
 const d = document;
-
-d.addEventListener('DOMContentLoaded', e=>{
-
-    const $table = d.querySelector('.crud-table');
-    const $form = d.querySelector('.crud-form');
-    const $title = d.querySelector('.crud-title');
-    const $template = d.getElementById('crud-template').content;
-    const $fragment = d.createDocumentFragment();
+const $table = d.querySelector('.crud-table');
+const $form = d.querySelector('.crud-form');
+const $title = d.querySelector('.crud-title');
+const $template = d.getElementById('crud-template').content;
+const $fragment = d.createDocumentFragment();
 
     // Subrutina para usar ajax
     const ajax = options =>{
@@ -23,20 +19,34 @@ d.addEventListener('DOMContentLoaded', e=>{
                 let json = JSON.parse(xhr.responseText);
                 success(json);
             }else{
-                let message = xhr.statusText || `<h2> Error : ${xhr.status} - Ocurrio un error !!!</h2>`
+                let message = xhr.statusText || `Error : ${xhr.status} - Ocurrio un error !!!`
+                console.warn(message);
                 error(message);
             }
 
         });
 
         xhr.open(method || "GET", url);
-
+        
         // Creamos la cabezera de la peticion (si nuestra peticion no tiene cabezera el JSON - server no funciona)
         xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
-
+        
         xhr.send(JSON.stringify(data));
     }
+    
+    const getAllProgrammers = ()=>{
 
-});
-
-
+        ajax({
+            url: "http://localhost:5001/prog0rammers/",
+            success : response =>{
+                console.log(response);
+            },
+            error : message => {
+                console.log(message);
+                let p = `<p>${message}</p>`
+                $table.insertAdjacentHTML("afterend", p);
+            }
+        });
+    }
+    
+d.addEventListener('DOMContentLoaded', getAllProgrammers);
