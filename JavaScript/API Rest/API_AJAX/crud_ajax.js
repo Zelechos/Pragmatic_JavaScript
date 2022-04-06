9ff4e6cb6b19ccc93b748fcbad23d22d77431a54
Pragmatic Coder <zelechos@hotmail.com>
2022-04-06 02:05:00 -0400

    v0.2.8

--- JavaScript/API Rest/API_AJAX/crud_ajax.js
+++ JavaScript/API Rest/API_AJAX/crud_ajax.js
@@ -1,3 +1,4 @@
+// CRUD ===> API Rest con AJAX
 const d = document;
 const $table = d.querySelector('.crud-table');
 const $form = d.querySelector('.crud-form');
@@ -49,18 +50,91 @@ const $fragment = d.createDocumentFragment();
 
                     //- Sin Embargo para eliminar los datos solo necesitamos el id
                     $template.querySelector('.delete').dataset.id = programmer.id;
-                    
+                    // Este es un agregado para el mensaje de confirmacion para eliminar
+                    $template.querySelector('.delete').dataset.name = programmer.name;
+
                     let $clone = d.importNode($template, true);
                     $fragment.appendChild($clone);
                 });
 
                 $table.querySelector("tbody").appendChild($fragment);
             },
-            error : message => {
+            error : message =>{
                 let p = `<p>${message}</p>`
                 $table.insertAdjacentHTML("afterend", p);
             }
         });
     }
-    
+
+// Traemos este evento para leer los datos de nuestra API rest
 d.addEventListener('DOMContentLoaded', getAllProgrammers);
+
+// Creamos un evento para la operacion submit del formulario 
+d.addEventListener('submit', e=>{
+    if(e.target === $form){
+        e.preventDefault();
+        
+        // Verificamos si existe el valor para hacer la posicion POST [para crear un dato]
+        if(!e.target.id.value){//POST ==> CREATE
+            console.warn(`[mode ==>[POST]<== activate]`);
+            ajax({
+                url: "http://localhost:5001/programmers/",
+                method: "POST",
+                success: response => location.reload(),
+                error: message =>{
+                    let p = `<p>${message}</p>`
+                    $form.insertAdjacentHTML("afterend", p);
+                },
+                data: {
+                    name : e.target.name.value,
+                    profession : e.target.profession.value
+                }
+            });
+            
+        }else{//PUT ==> UPDATE (haremos una peticion para actualizar)
+            console.warn(`[mode ==>[PUT]<== activate]`);
+            ajax({
+                url: `http://localhost:5001/programmers/${e.target.id.value}`,
+                method: "PUT",
+                success: response => location.reload(),
+                error: message =>{
+                    let p = `<p>${message}</p>`
+                    $form.insertAdjacentHTML("afterend", p);
+                },
+                data: {
+                    id: e.target.id.value,
+                    name : e.target.name.value,
+                    profession : e.target.profession.value
+                }
+            });
+        }
+    }
+
+});
+
+
+// Evento para los botones de eliminar y editar
+d.addEventListener('click', e=>{
+    if(e.target.matches(".edit")){
+        $title.textContent = "Edit Programmer";
+        $form.name.value = e.target.dataset.name;
+        $form.profession.value = e.target.dataset.profession;
+        $form.id.value = e.target.dataset.id;
+    }
+
+    if(e.target.matches(".delete")){
+        let response = confirm(`Are you sure to remove the programmer ${e.target.dataset.name} ?`) 
+        if(response){
+            // DELETE => DELETE
+            ajax({
+                url: `http://localhost:5001/programmers/${e.target.dataset.id}`,
+                method: "DELETE",
+                success: response => location.reload(),
+                error: message =>{
+                    let p = `<p>${message}</p>`
+                    $form.insertAdjacentHTML("afterend", p);
+                }
+            });
+        }
+    }
+});
