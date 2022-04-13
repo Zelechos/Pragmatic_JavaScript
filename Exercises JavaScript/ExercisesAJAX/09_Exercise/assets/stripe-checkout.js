685aa495accefb42e753e440d3d2d12d779d9102
Pragmatic Coder <zelechos@hotmail.com>
2022-04-12 23:16:38 -0400

    v0.2.9

--- Exercises JavaScript/ExercisesAJAX/09_Exercise/assets/stripe-checkout.js
+++ Exercises JavaScript/ExercisesAJAX/09_Exercise/assets/stripe-checkout.js
@@ -13,6 +13,8 @@ const stripeAuthorization = {
     }
 };
 
+const formatMoney = price => `$${price.slice(0,-2)}.${price.slice(-2)}`;
+
 let prices, products;
 
 // El método Promise.all(iterable) devuelve una promesa que termina correctamente cuando todas las promesas en el argumento iterable han sido concluídas con éxito, o bien rechaza la petición con el motivo pasado por la primera promesa que es rechazada.
@@ -31,20 +33,23 @@ Promise.all([
         // Creando las tarjetas en html
         prices.forEach(price =>{
             let productData = products.filter(product => product.id === price.product);
-            console.log(productData);
-            $template.querySelector(".course").setAttribute("data-price", price.id);
-            $template.querySelector('img').setAttribute('src', productData[0].images[0]);
-            $template.querySelector('img').setAttribute('alt', productData[0].name);
-            $template.querySelector("figcaption").innerHTML = `
-                <h2 class="tittle">${productData[0].name}</h2>
-                <h4 class="subtittle">${productData[0].description}</h4>
-                <h4 class="prices">$${price.unit_amount / 100}.00 ${(price.currency).toUpperCase()}</h4>
+
+            //Hacemos esto porque no puedo eliminar el precio erroneo
+            if(price.id !== "price_1KnbNfF3EePOWwxlveNT63M7"){
+                console.log(productData);
+                $template.querySelector(".course").setAttribute("data-price", price.id);
+                $template.querySelector('img').setAttribute('src', productData[0].images[0]);
+                $template.querySelector('img').setAttribute('alt', productData[0].name);
+                $template.querySelector("figcaption").innerHTML = `
+                ${productData[0].name}<br>
+                ${productData[0].description}<br>
+                ${formatMoney(price.unit_amount_decimal)} ${(price.currency).toUpperCase()}<br>
+                `;
                 
-            `;
-            
-            // Manera de insertar el contenido del template a un elemento
-            let $clone = d.importNode($template, true);
-            $fragment.appendChild($clone);
+                // Manera de insertar el contenido del template a un elemento
+                let $clone = d.importNode($template, true);
+                $fragment.appendChild($clone);
+            }
         });
 
         $courses.appendChild($fragment);
@@ -55,7 +60,34 @@ Promise.all([
     })
 
 
+    // Creamos un evento click para las tarjetas para la comprar de productos
+    d.addEventListener('click', e=>{
 
+        if(e.target.matches(".course *")){
+            let priceId = e.target.parentElement.getAttribute("data-price");
+            console.log(priceId);
+            // Invocamos el objeto Stripe y le pasamos como parametro la llave publica
+            // llamamos a su metodo redirectToCheckout(); para redirigir la web a la pasarela de pagos 
+            // el metodo redirectToCheckout() tiene como parametro un objeto de configuracion que es el siguiente!!
+            Stripe(STRIPE_KEYS.public)
+            .redirectToCheckout({
+                lineItems:[{price: priceId, quantity: 1 }],
+                mode : "payment",
+                successUrl : "http://127.0.0.1:5500/Exercises%20JavaScript/ExercisesAJAX/09_Exercise/assets/stripe-success.html",
+                cancelUrl : "http://127.0.0.1:5500/Exercises%20JavaScript/ExercisesAJAX/09_Exercise/stripe-cancel.html"
+            })
+            .then(response => {
+                console.log(response);
+                if(response.error){
+                    $courses.insertAdjacentHTML('afterend', response.error.message);
+                }
+            })
+        }
+
+
+
+
+    })
 // GRACIAS al metodo Promise.all(iterable); ya no necesitamos las dos peticiones que hicimos anteriormente
 
 // Peticion para obtener los datos como nombre, descripcion , imagenes de los cursos
